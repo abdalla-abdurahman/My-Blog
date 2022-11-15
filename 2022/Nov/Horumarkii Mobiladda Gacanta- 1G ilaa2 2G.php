c2f2007066f2d4b6a1a1719eba23fb57776397a0
abdalla-abdurahman <66676329+abdalla-abdurahman@users.noreply.github.com>
2022-11-15 11:25:12 +0300

    Add files via upload

---
+++ 2022/Nov/Horumarkii Mobiladda Gacanta- 1G ilaa2 2G.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Post Page</title>
+    <!-- Link To CSS -->
+    <link rel="stylesheet" href="../../css/post-style.css">
+    <!-- Box Icons -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <!-- Link To JS -->
+
+    <!-- jQuerry Link  -->
+    <script src="jQuerry/jquery-3.5.1.min.js"></script>
+</head>
+
+<body>
+    <!-- Header -->
+    <header>
+        <!-- Nav -->
+        <div class="nav container">
+            <!-- logo -->
+            <a href="#" class="logo">Ab<span>dalla</span></a>
+
+        </div>
+
+    </header>
+    <!-- Post Content  -->
+    <section class="post-header">
+        <div class="header-content post-container">
+            <!-- Back Home  -->
+            <a href="../../index.php" class="back-home"> Back To Home</a>
+            <!-- Title  -->
+            <h1 class="header-title"> Horumarkii Mobiladda Gacanta- 1G ilaa 2G</h1>
+            <!-- Post Image  -->
+            <img src="../../img/01_mobira_talkman.jpg" alt="" class="header-img">
+        </div>
+    </section>
+
+
+
+    <!-- Posts -->
+    <section class="post-content post-container">
+        <h2 class="sub-heading"></h2>
+        <p class="post-text">Tiknoolajiyada mobiladu waxay soo martay jiilal kala duwan intii ay jirtay. Jiilashaa (Generations) oo loo soo gaabiyo xarafka G sida 1G, 2G, 3G, 4G iyo 5G, waxa ay ku kala duwan yihiin adeegyada iyo qaabdhismeedka network-ga. Mobile Phone ama Cell Phone sida meelaha qaar looga yaqaano waa aalad qofku sidan karo oo loo isticmaalo in ay dadku ku wada xidhiidhan cod (voice), qoraal (text) ama muuqaal (video) sida hadda jirta.</p>
+
+        <img src="../../img/Nokia 3210.JPG" alt="" srcset="" class="post-img"> <br><br>
+        <p class="post-text">
+            Hadaba, 1G oo ahaa jiilka ugu horeeyay ee telefoonada gacanta lagu qaato waxa la hirgaliyay isticmaalkiisa 1980. Maadama oo bilaw ahayd tiknoolajiyadani, wadamo badan oo ku hawlanaa tiknoolajiyadani ayaa mid kastaaba iskii u samaystay. Wadamadaa Nordic ee yurubta waqooyi waxay samaysteen NMTs (Nordic Mobile Telephones), Uk Total Access Comunication System (TACS), sidoo kale U.S. Advance Mobile Phone System (AMPS).
+            1G waxa uu ahaa analogy system oo keena in tayada codku xumaado markasta oo masaafadu sii dheerato iyo in signalka uu ku bato waxa loo yaqaano interference. Dhibta kale uu laha ayaa waxa ka mida ma ay isticmaali karayn dad badani hal mar. Tusaale; hadii ay isku dayaan dad badani inay hal mar wicitaan sameeyaan ma ay u suurto geli karayn, sababta oo ah waxa xadidnaa janallada loogu talo galay in ay qaadan hadalka, dadka Frequency Division Multiple Access (FDMA). Ta kale, is ma ay geli karayn systems ka kala duwan ee wadan kasta u lahaa sida qofka UK jooga ee isticmaalaya TACS lama u xidhiidhi karayn ka U.S. jooga ee isticmaalaya AMPS. </p>
+
+        <img src="../../img/nokia mobira senator.jpg" alt="" srcset="" class="post-img"> <br><br>
+        <br>
+        <p class="post-text">
+            Waxa hadaba muhiim noqotay si loo yareeyo dhibaatoyinka u lahaa 1G in la sameeyo jiilka labaad ee ama second generation (2G). Second generation waxa u xaliyay dhibaatoyin badan sida in uu kordhiyay tirada subscribers ka ee mar keliya isticmaali kara, in tayada codka uu fiicnaado iyada oo loo badalayo digital signal. Adeega fariimaha qoraalka ee Short Message Service (SMS) ayaa isna ka mid ahaa adeegyada uu 2G-gu soo kordhiyay. Waxa kale markii danbe lagu soo daray adeega loo yaqaano General Packet Radio System (GPRS) oo u saamaxay in lagu isticmaalo mobile-ka internet-ka markii ugu horraysay u soo kordhiyay adeegyo cusub.
+            <br>
+
+            Intaas oo faa’ido oo 2G-gu soo kordhiyay ayaa keentay in markiiba laga guuro system kii horee jiray ee 1G. Ka hore intii aan la hirgalin samaynta 2G-ga, Europe oo badanka hogaamiysay dhanka tiknoolajiyadda mobillada, ayaa isku raacday in ay sameeyan hay’ad u qaabilsan dejinta nidaamyada iyo protocols-ka uu lahaanayo jiilka labaad ee mobiladu halkii markii hore wadan kastaaba iskii u samysanayay. Hay’adan oo lagu magacaabo European Telecommunications Standards Institute (ETSI) waxa ay soo saartay Global System for Mobile Communication (GSM) oo ah hab qeexaaya sida uu shaqaynayo iyo astaamaha uu leeyahay 2G-gu. Taasi waxay suurto galisay adeega la yidhaa roaming-ka oo 1G suurto gal ahayn maadaama oo ay kala duwanaayeen hirarka (frequency) ay kala isticmaalayaan systemyadu, in uu noqdo faaido kale oo uu keenay GSM.
+            Hase ahaate, North America iyadu waxa ay si horumarisay AMPS kana dhigtay 2G oo noqday D-AMPS. Inta kalee aduunka waxay wada qaateen GSM-ka.
+
+        </p>
+
+        <br> <br> <br>
+
+        <!-- Share  -->
+        <div class="share post-container">
+            <span class="share-title">Share this article</span>
+            <div class="social">
+                <a href=""><i class='bx bxl-facebook'></i></a>
+                <a href=""><i class='bx bxl-twitter'></i></a>
+                <a href=""><i class='bx bxl-linkedin'></i></a>
+                <a href=""><i class='bx bxl-instagram'></i></a>
+
+            </div>
+        </div>
+
+    </section>
+    <!-- Footer  -->
+    <div class="footer container">
+        <p>&#169; Abdalla All Right Reserved</p>
+        <div class="social">
+            <a href=""><i class='bx bxl-facebook'></i></a>
+            <a href=""><i class='bx bxl-twitter'></i></a>
+            <a href=""><i class='bx bxl-linkedin'></i></a>
+            <a href=""><i class='bx bxl-instagram'></i></a>
+
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+    <!-- Link To JS -->
+    <script src="js/main.js"></script>
+    <script>
+
+    </script>
+    <script type="text/javascript">
+
+    </script>
+
+
+</body>
+
+</html>
---
+++ 2022/Nov/Imisa Nooc oo Cryptocurrency Ayaa Jira.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Post Page</title>
+    <!-- Link To CSS -->
+    <link rel="stylesheet" href="../../css/post-style.css">
+    <!-- Box Icons -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <!-- Link To JS -->
+
+    <!-- jQuerry Link  -->
+    <script src="jQuerry/jquery-3.5.1.min.js"></script>
+</head>
+
+<body>
+    <!-- Header -->
+    <header>
+        <!-- Nav -->
+        <div class="nav container">
+            <!-- logo -->
+            <a href="#" class="logo">Ab<span>dalla</span></a>
+
+        </div>
+
+    </header>
+    <!-- Post Content  -->
+    <section class="post-header">
+        <div class="header-content post-container">
+            <!-- Back Home  -->
+            <a href="../../index.php" class="back-home"> Back To Home</a>
+            <!-- Title  -->
+            <h1 class="header-title"> Imisa Nooc oo Cryptocurrency Ayaa Jira</h1>
+            <!-- Post Image  -->
+            <img src="../../img/cryptocurrency.jpg" alt="" class="header-img">
+        </div>
+    </section>
+
+
+
+    <!-- Posts -->
+    <section class="post-content post-container">
+        <h2 class="sub-heading">Waxaa laga yaabaa inaad la yaabto inta 'cryptocurrencies' ee jira iyo sida dhakhsaha ah ee tiradu u kordheyso</h2>
+        <p class="post-text">Waxaa hadda jira in ka badan 12,000 cryptocurrencies, iyo waxa run ahaantii la yaab leh waa heerka koritaanka. Tirada cryptocurrencies in ka badan labanlaabmay laga bilaabo 2021 ilaa 2022. Dhammaadkii 2021, suuqa ayaa ku daray ku saabsan 1,000 cusub cryptocurrencies bil kasta.
+
+            Tani gabi ahaanba ma aha war wanaagsan. Qaar badan oo ka mid ah lacagaha cusub ee loo yaqaan 'cryptocurrencies' ayaa leh ujeedo yar oo aan ahayn inay lacag u sameeyaan horumariyayaashooda, taas oo macnaheedu yahay maalgashadayaasha waxay u baahan yihiin inay doortaan. Kaliya qayb yar oo ka mid ah lacagaha loo yaqaan 'cryptocurrencies' ayaa mudan in wax laga barto oo ay suurtagal tahay in la iibsado.
+
+            Waa maxay sababta tirada lacagta loo yaqaan 'cryptocurrencies' ay si degdeg ah u koraysay? Waxaan taas ka jawaabi doonaa oo hoos ku dabooli doonaa lacagaha ugu muhiimsan uguna saamaynta badan cryptocurrencies.
+            Waa maxay sababta ay u jiraan cryptocurrencies badan oo kala duwan?
+            Sababta ugu weyn ee ay u jiraan cryptocurrencies kala duwan ayaa ah in aysan jirin wax caqabad ah oo lagu galo. Qof kasta oo raba inuu abuuro cryptocurrency ayaa sameyn kara. Xitaa haddii aad leedahay aqoon farsamo oo eber ah, waxaad ku shaqaaleysiin kartaa qof Fiverr si aad cryptocurrency ugu sameyso wax ka yar $20.
+
+            Mar walba sidan may ahayn. Maalmihii hore, waxaa jiray kaliya Bitcoin. Kadib horumariyayaashu waxay bilaabeen abuuritaanka altcoins. Altcoin waa cryptocurrency kasta oo aan ahayn Bitcoin. Inta badan altcoins hore waxaa loogu talagalay in lagu hagaajiyo waxqabadka Bitcoin ama u adeego ujeedo kale.
+            Inta badan lacagaha loo yaqaan 'cryptocurrencies' ayaa wali leh ujeedo ama hadaf. Horumariyayaashu waxay abuuraan cryptocurrencies iyagoo rajeynaya inay isticmaalaan tignoolajiyada blockchain si ay u xalliyaan dhibaatada dhabta ah ee aduunka.
+            Maadaama ay aad u fududahay in la sameeyo cryptocurrency, qadarka lacagta ee crypto ayaa soo jiidatay dadka isku dayaya inay sameeyaan lacag degdeg ah. Haddii aad eegto jaantusyada cryptocurrencies ee dhawaan la bilaabay, waxaad arki doontaa wax badan oo sidan oo kale ah, oo wata nooc ka mid ah "Elon," "Shib," ama "Doge" magaca.
+            Haddii aad raadinayso maalgashi wanaagsan oo cryptocurrency ah, ama haddii aad xiisaynayso inaad wax ka ogaato qaar ka mid ah mashaariicda caanka ah, halkan waxaa ah cryptocurrencies ee saamaynta ugu weyn ku yeeshay.
+
+
+        <h3> Bitcoin </h3>
+
+        <p class="post-text"> Bitcoin waxay ahayd cryptocurrency-gii ugu horreeyay ee adduunka. Aasaasaha aan la magacaabin oo la baxay magaca Satoshi Nakamoto ayaa bilaabay Bitcoin 2009. Waxaa loo qorsheeyay inay noqoto mid baahsan, lacag dijital ah oo aan ku tiirsanayn bangiyada ama hay'adaha maaliyadeed.
+            Lacagta cusub ee loo yaqaan 'cryptocurrencies' ayaa aad ugu horumarsan tignoolajiyada waxayna bixiyaan wax kala beddelasho hufan, sidaa darteed Bitcoin hadda waxa loo isticmaalaa kayd qiimo leh. Waxay ku raaxaysatay faa'iidada dhaqdhaqaaqa koowaad ee muhiimka ah maadaama ay tahay cryptocurrency ugu caansan uguna qiimaha badan marka loo eego xad ballaaran. </p>
+
+        <h3> Ethereum</h3>
+        <p class="post-text"> Ethereum waxay soo bandhigtay fikradda il furan, blockchain barnaamij la samayn karo. Horumariyayaashu waxay awoodaan inay ku dhistaan blockchain Ethereum si ay u sameeyaan calaamadahooda cryptocurrency iyo apps-ka baahsan (dApps). Tani waxay horseedday abuurista maaliyadda baahsan (DeFi) -- aaladaha bixiya noocyada adeegyada maaliyadeed ee dhaqameed ee baahsan.
+            Inkasta oo ay hadda jiraan lacago badan oo loo yaqaan 'cryptocurrencies' oo leh xannibaadyo barnaamijeed, Ethereum ayaa bilaabay dhammaan. Horumarinteedii hore ayaa ka caawisay inay si adag u noqoto cryptocurrency-ka labaad ee ugu weyn.</p>
+
+        <h3>Ku xidhid</h3>
+        <p class="post-text"> Tether waa stakecoin ugu weyn, kaas oo ah nooc ka mid ah cryptocurrency loogu talagalay in lagu raaco qiimaha hantida kale. Xaaladda Tether, waxay ujeedadeedu tahay inay raacdo qiimaha doolarka Maraykanka, taasoo la micno ah hal Tether caadi ahaan qiimaheedu waa $1.
+            cryptocurrency Tani ma aha muran la'aan. Shirkadda ka dambeysa, Tether Limited, ayaa been ka sheegtay kaydkeeda markii ay been abuur ku sheegtay in Tether kasta uu taageeray doolarka Maraykanka. Iyadoo ay taasi jirto iyo arrimo kale, Tether caadi ahaan waa cryptocurrency oo leh mugga ganacsiga ugu weyn ee maalinlaha ah.
+        </p>
+        <br> <br> <br>
+
+
+        <!-- Share  -->
+        <div class="share post-container">
+            <span class="share-title">Share this article</span>
+            <div class="social">
+                <a href=""><i class='bx bxl-facebook'></i></a>
+                <a href=""><i class='bx bxl-twitter'></i></a>
+                <a href=""><i class='bx bxl-linkedin'></i></a>
+                <a href=""><i class='bx bxl-instagram'></i></a>
+
+            </div>
+        </div>
+
+    </section>
+    <!-- Footer  -->
+    <div class="footer container">
+        <p>&#169; Abdalla All Right Reserved</p>
+        <div class="social">
+            <a href=""><i class='bx bxl-facebook'></i></a>
+            <a href=""><i class='bx bxl-twitter'></i></a>
+            <a href=""><i class='bx bxl-linkedin'></i></a>
+            <a href=""><i class='bx bxl-instagram'></i></a>
+
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+    <!-- Link To JS -->
+    <script src="js/main.js"></script>
+    <script>
+
+    </script>
+    <script type="text/javascript">
+
+    </script>
+
+
+</body>
+
+</html>

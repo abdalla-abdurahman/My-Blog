e29fa8a3bcc8b51172d76c9bfd2e0de307aa3766
abdalla-abdurahman <66676329+abdalla-abdurahman@users.noreply.github.com>
2022-11-15 11:07:21 +0300

    Add files via upload

---
+++ post-page.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Post Page</title>
+    <!-- Link To CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Box Icons -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <!-- Link To JS -->
+
+    <!-- jQuerry Link  -->
+    <script src="jQuerry/jquery-3.5.1.min.js"></script>
+</head>
+
+<body>
+    <!-- Header -->
+    <header>
+        <!-- Nav -->
+        <div class="nav container">
+            <!-- logo -->
+            <a href="#" class="logo">Ab<span>dalla</span></a>
+            <!-- Login Btn -->
+            <a href="#" class="login"> login</a>
+        </div>
+
+    </header>
+    <!-- Post Content  -->
+    <section class="post-header">
+        <div class="header-content post-container">
+            <!-- Back Home  -->
+            <a href="index.php" class="back-home"> Back To Home</a>
+            <!-- Title  -->
+            <h1 class="header-title"> Imisa Nooc oo Cryptocurrency Ayaa Jira</h1>
+            <!-- Post Image  -->
+            <img src="img/bitcoin 3.jpg" alt="" class="header-img">
+        </div>
+    </section>
+
+
+
+    <!-- Posts -->
+    <section class="post-content post-container">
+        <h2 class="sub-heading">Create Besct Ux Design</h2>
+        <p class="post-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos illum iure quam officiis corrupti animi culpa eos dolore optio quasi sed cupiditate labore, aperiam, eius id dolorum odit maiores eligendi! Necessitatibus rem odit animi ipsa nostrum optio saepe voluptates natus magni voluptatibus, et iusto rerum quas enim, explicabo, perferendis facilis laudantium beatae dolore. Modi laboriosam reiciendis sed, sint odio eligendi perspiciatis tenetur, molestiae dicta velit quaerat repellendus. Commodi quisquam rem mollitia in iste neque eius. Nesciunt, suscipit assumenda quod ullam odit laudantium vel molestias! Veniam obcaecati fuga magnam porro mollitia dignissimos, consequuntur vitae esse voluptates, suscipit qui error tempore vel?</p>
+        <h2 class="sub-heading">Create sub</h2>
+        <p class="post-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo, veritatis mollitia! Maxime ipsum, magnam optio labore, pariatur ducimus excepturi dolore rerum nulla dignissimos laudantium nesciunt modi qui blanditiis expedita. Blanditiis tenetur similique dolorem voluptates minus. Cumque dolore blanditiis corporis fugit eius accusamus magnam quasi dicta maxime quos ut, tenetur quas officiis animi libero iusto veniam facilis nisi voluptatum. Esse necessitatibus earum aliquid vitae repellat, nostrum velit eos ipsa voluptate. Officia, nam non fuga nemo sint nulla incidunt! Voluptate id libero ab perspiciatis quas fuga cumque, veniam mollitia ducimus impedit nisi, iste ad distinctio, aut inventore provident cupiditate ipsum quis itaque.</p>
+
+        <!-- Share  -->
+        <div class="share post-container">
+            <span class="share-title">Share this article</span>
+            <div class="social">
+                <a href=""><i class='bx bxl-facebook'></i></a>
+                <a href=""><i class='bx bxl-twitter'></i></a>
+                <a href=""><i class='bx bxl-linkedin'></i></a>
+                <a href=""><i class='bx bxl-instagram'></i></a>
+
+            </div>
+        </div>
+
+    </section>
+    <!-- Footer  -->
+    <div class="footer container">
+        <p>&#169; Abdalla All Right Reserved</p>
+        <div class="social">
+            <a href=""><i class='bx bxl-facebook'></i></a>
+            <a href=""><i class='bx bxl-twitter'></i></a>
+            <a href=""><i class='bx bxl-linkedin'></i></a>
+            <a href=""><i class='bx bxl-instagram'></i></a>
+
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+    <!-- Link To JS -->
+    <script src="js/main.js"></script>
+    <script>
+
+    </script>
+    <script type="text/javascript">
+
+    </script>
+
+
+</body>
+
+</html>
